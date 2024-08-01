c6e7e9dba7cfc8b334a172603f0085f031c8f377
Kishan pithiya <pithiyakishan9437@gmail.com>
2024-08-01 19:26:09 +0530

    second commit

--- full-throttle/src/pages/Home.js
+++ full-throttle/src/pages/Home.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Container } from 'react-bootstrap'
 import '../style/Home.css'
 import Section1 from '../component/Section1'
+import Section2 from '../component/Section2'
 
 
 export default function Home() {
@@ -11,6 +12,7 @@ export default function Home() {
         <div className='bg-gray'>
           <Container>
             <Section1 />
+            <Section2/>
           </Container>
         </div>
       </article>
